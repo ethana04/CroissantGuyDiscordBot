116f2d93c83e9f0b388c08757a74c4104e3772a8
ethana04 <emma.29@outlook.fr>
2025-01-31 23:23:54 +0100

    Bot creation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CroissantGuyDiscordBot
-Just a lil guy trying to help
+Just a lil guy trying to help =)
+
+
+## Requirements
+- Python
+- environment variable : "TOKEN_BOT" (ask Emma for the token)
+
+## Installation
+- Get the code (duh)
+- Add the token to the env. variables
+- ``` python main.py ```
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import discord
+import json, random
+from discord.ext import commands
+
+# Get the token from the environment variable
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Link to json file
+links = json.load(open("gifs.json"))
+
+# Define bot instance
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+# Commands
+@bot.command()
+async def bonjour(ctx):
+  await ctx.send(f"Bonjour {ctx.author} !")
+
+@bot.command()
+async def yell(ctx):
+  await ctx.send("AAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH !")
+
+@bot.command()
+async def gymyell(ctx):
+  await ctx.send("GO TO THE GYM ! NOW !")
+
+@bot.command(name="gif", aliases=["guillotine"])
+async def gifs(ctx):
+  await ctx.send(random.choice(links[ctx.invoked_with]))
+
+@bot.event
+async def on_message(message):
+  if "cannibalism" in message.content :
+    await message.channel.send("https://media.discordapp.net/attachments/1007598476504338465/1334936943368081539/20240922_123137.jpg?ex=679e5869&is=679d06e9&hm=207216826a61fc8cc4de1ba93552da95d0984dc5ce666acffa9415b8f222b006&=&format=webp")
+
+
+
+# Run bot
+token = os.environ['TOKEN_BOT']
+bot.run(token)
