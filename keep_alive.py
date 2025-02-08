cf06b9464f7f7ee7a0a07fb6fbd36eefd716949a
ethana04 <emma.29@outlook.fr>
2025-02-08 13:40:07 +0100

    fix keep_alive

--- keep_alive.py
+++ keep_alive.py
@@ -5,8 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
-  return "The croissant is in the boulangerie !"
+  return "La bot est en ligne !"
 
 def run():
   app.run(host='0.0.0.0', port=8080)
@@ -14,13 +13,3 @@ def run():
 def keep_alive():
   t = Thread(target=run)
   t.start()
-=======
-    return "Croissant is online !"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t=Thread(target=run)
-    t.start()
->>>>>>> d1dffe763f336615c37f5a03a5667d298e7c2bf6
