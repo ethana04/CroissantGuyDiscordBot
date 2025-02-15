fef8803e8b107ef249d8c2ba8353a8e42386758e
ethana04 <emma.29@outlook.fr>
2025-02-15 16:52:57 +0100

    fix spaces events

--- cogs/events.py
+++ cogs/events.py
@@ -16,27 +16,32 @@ class EventsCog(commands.Cog) :
         embed1 = discord.Embed(colour = getDefaultEmbedColour())
         embed1.set_image(url="attachment://miss_france.gif")
         await message.channel.send(file = file1, embed=embed1)
+
       if "CANNIBALISM" in message.content.upper() :
         file2 = discord.File("media/cannibalism.jpg", filename="cannibalism.jpg")
         embed2 = discord.Embed(colour = getDefaultEmbedColour())
         embed2.set_image(url="attachment://cannibalism.jpg")
         await message.channel.send(file = file2, embed=embed2)
-      if " TREE " in message.content.upper() :
+
+      if message.content.upper() == "TREE" or message.content.upper().startswith("TREE ") or message.content.upper().endswith(" TREE") or " TREE " in message.content.upper() :
         embed3 = discord.Embed(colour = getDefaultEmbedColour(), title="TREE ?", description="Someone said tree ?  <@1007600320001613887>")
         embed3.add_field(name="", value="The tree is here : <#1007660806118309961>", inline=False)
         await  message.channel.send(embed=embed3)
-      if "AHH" in message.content.upper() :
+
+      if "AHHH" in message.content.upper() :
         file4 = discord.File("media/goat.jpg", filename="goat.jpg")
         embed4 = discord.Embed(colour = getDefaultEmbedColour(), title="AAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
         # embed4.set_thumbnail(url="attachment://goat.jpg")
         # await message.channel.send(file=file4, embed=embed4)
         await message.channel.send(embed=embed4)
+
       if message.author.name == "suitcasewean" and  " ANANAS" in message.content.upper():
         file5 = discord.File("media/ananas.gif", filename="ananas.gif")
         embed5 = discord.Embed(colour = getDefaultEmbedColour())
         embed5.set_image(url="attachment://ananas.gif")
         await message.channel.send(file = file5, embed=embed5)
-      if " OLA " in message.content.upper() :
+
+      if message.content.upper() == "OLA" or message.content.upper().startswith("OLA ") or message.content.upper().endswith(" OLA") or " OLA " in message.content.upper() :
         file6 = discord.File("media/nerd_face.jpg", filename="nerd_face.jpg")
         embed6 = discord.Embed(colour = getDefaultEmbedColour(), title="You spelled Hola wrong !")
         # embed6.set_image(url="attachment://nerd_face.jpg")
